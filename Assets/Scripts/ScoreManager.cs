41cae61c85c276796d1770331f21be13b084e3ac
jktamang <49430416+jktamang@users.noreply.github.com>
2019-04-16 16:27:25 +0800

    Added scoring mechanism

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // float score;
+    int initialPositionX;
+    GameObject body;
+    Text scoreDisplay;
+    
+    void Start()
+    {
+        scoreDisplay = GameObject.Find("Score").GetComponent<Text>();
+        body = GameObject.Find("Body");
+        initialPositionX = (int)body.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreDisplay.text = ((int)body.transform.position.x - initialPositionX).ToString();
+    }
+}
