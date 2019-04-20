743ee49061a8343aa386867cd0edb663296d99be
jktamang <49430416+jktamang@users.noreply.github.com>
2019-04-21 05:35:57 +0800

    Changed art direction for UI Images Added toggle for Credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    GameObject creditsText;
+    bool showCredits = false;
+
+    void Start()
+    {
+        creditsText = GameObject.Find("Credits");
+        creditsText.SetActive(false);
+    }
+
+    public void ToggleCredits()
+    {
+        if (showCredits)
+        {
+            creditsText.SetActive(false);
+            showCredits = false;
+        }
+        else
+        {
+            creditsText.SetActive(true);
+            showCredits = true;
+        }
+    }
+}
