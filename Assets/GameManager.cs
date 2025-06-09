ef643aed32477fdb14498db8cd6560c0edf53f80
bztamang <kenneth@boomzap.com>
2025-06-09 13:00:23 +0800

    Add controller support.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,7 @@ public class GameManager : SimpleSingleton<GameManager>
 {
     public float currentTimeScale = 1.0f;
     public float timeScaleStep = 0.05f;
+    [SerializeField] FPSManager fpsManager;
 
     [SerializeField]
     public int[] scoreThreshold = {200, 300, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000};
@@ -13,13 +14,18 @@ public class GameManager : SimpleSingleton<GameManager>
     void Start()
     {
         currentThreshold = 0;
+#if MOBILE_INPUT
+        fpsManager.gameObject.SetActive(true);
+#else
+        fpsManager.gameObject.SetActive(false);
+#endif
     }
 
     // Update is called once per frame
     void Update()
     {
         if (UIManager.instance.IsRulesActive()) return;
-
+        if (UIManager.instance.IsGameOverActive()) return;
         if (CrossPlatformInputManager.GetButtonDown("Cancel"))
         {
             PauseMenu.instance.TogglePause();
--- Assets/GameOverUI.cs
+++ Assets/GameOverUI.cs
@@ -10,7 +10,6 @@ public class GameOverUI : SimpleSingleton<GameOverUI>
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
